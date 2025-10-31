aac0f088f749fffd9ab23838f047334f098b9ede
이주호 <tiger98210@gmail.com>
2025-10-31 11:06:40 +0900

    공통파일 업로드입니다.
    공통파일 업로드입니다.

---
+++ src/main/java/com/palceeat/utils/BoardPage.java
@@ -0,0 +1,96 @@
+package com.palceeat.utils;
+
+public class BoardPage {
+	// 페이징 처리 메서드
+	// totalCount : 전체 게시물 수
+	// pageSize : 한 페이지당 출력할 게시물 수
+	// blockPage : 한 블럭당 출력할 페이지 수
+	// pageNum : 현재 페이지 번호
+    public static String pagingStr(int totalCount, int pageSize, int blockPage,
+            int pageNum, String reqUrl) {
+        String pagingStr = "";
+
+        // 단계 3 : 전체 페이지 수 계산
+        int totalPages = (int) (Math.ceil(((double) totalCount / pageSize)));
+
+        // 단계 4 : '이전 페이지 블록 바로가기' 출력
+        int pageTemp = (((pageNum - 1) / blockPage) * blockPage) + 1;
+        if (pageTemp != 1) {
+            pagingStr += "<a href='" + reqUrl + "?pageNum=1'>[첫 페이지]</a>";
+            pagingStr += "&nbsp;";
+            pagingStr += "<a href='" + reqUrl + "?pageNum=" + (pageTemp - 1)
+                         + "'>[이전 블록]</a>";
+        }
+
+        // 단계 5 : 각 페이지 번호 출력
+        int blockCount = 1;
+        // blockPage 수만큼. 마지막 페이지까지만 출력
+        while (blockCount <= blockPage && pageTemp <= totalPages) {
+            if (pageTemp == pageNum) {
+                // 현재 페이지는 링크를 걸지 않음
+                pagingStr += "&nbsp;" + pageTemp + "&nbsp;";
+            } else {
+                pagingStr += "&nbsp;<a href='" + reqUrl + "?pageNum=" + pageTemp
+                              +"'>" + pageTemp + "</a>&nbsp;";
+            }
+            pageTemp++;
+            blockCount++;
+        }
+
+        // 단계 6 : '다음 페이지 블록 바로가기' 출력
+        if (pageTemp <= totalPages) {
+            pagingStr += "<a href='" + reqUrl + "?pageNum=" + pageTemp
+                         + "'>[다음 블록]</a>";
+            pagingStr += "&nbsp;";
+            pagingStr += "<a href='" + reqUrl + "?pageNum=" + totalPages
+                         + "'>[마지막 페이지]</a>";
+        }
+
+        return pagingStr;
+    }
+    
+    // reply용.오버로딩
+    // idx : 부모글 번호. 추가
+    public static String pagingStr(int totalCount, int pageSize, int blockPage,
+    		int pageNum, String reqUrl,int idx) {
+    	String pagingStr = "";
+    	
+    	// 단계 3 : 전체 페이지 수 계산
+    	int totalPages = (int) (Math.ceil(((double) totalCount / pageSize)));
+    	
+    	// 단계 4 : '이전 페이지 블록 바로가기' 출력
+    	int pageTemp = (((pageNum - 1) / blockPage) * blockPage) + 1;
+    	if (pageTemp != 1) {
+    		pagingStr += "<a href='" + reqUrl + "?pageNum=1'>[첫 페이지]</a>";
+    		pagingStr += "&nbsp;";
+    		pagingStr += "<a href='" + reqUrl + "?pageNum=" + (pageTemp - 1)
+    				+ "'>[이전 블록]</a>";
+    	}
+    	
+    	// 단계 5 : 각 페이지 번호 출력
+    	int blockCount = 1;
+    	// blockPage 수만큼. 마지막 페이지까지만 출력
+    	while (blockCount <= blockPage && pageTemp <= totalPages) {
+    		if (pageTemp == pageNum) {
+    			// 현재 페이지는 링크를 걸지 않음
+    			pagingStr += "&nbsp;" + pageTemp + "&nbsp;";
+    		} else {
+    			pagingStr += "&nbsp;<a href='" + reqUrl + "?pageNum=" + pageTemp
+    					+"&idx="+idx +"'>" + pageTemp + "</a>&nbsp;";
+    		}
+    		pageTemp++;
+    		blockCount++;
+    	}
+    	
+    	// 단계 6 : '다음 페이지 블록 바로가기' 출력
+    	if (pageTemp <= totalPages) {
+    		pagingStr += "<a href='" + reqUrl + "?pageNum=" + pageTemp
+    				+ "'>[다음 블록]</a>";
+    		pagingStr += "&nbsp;";
+    		pagingStr += "<a href='" + reqUrl + "?pageNum=" + totalPages
+    				+ "'>[마지막 페이지]</a>";
+    	}
+    	
+    	return pagingStr;
+    }
+}
